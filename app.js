750004b98e869dd3992bf61717181a2fefe11315
cansub1974 <jasonalexandermckay@hotmail.com>
2019-05-14 08:01:53 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/booking', (req, res) => {
+    res.render('booking');
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server has started successfully!");
+});
